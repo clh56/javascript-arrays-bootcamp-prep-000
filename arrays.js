52463cc39951fa34e9e515a427b8b5bfd00f831e
ciara hudson <cchudson33@gmail.com>
2017-09-20 19:19:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,42 @@
-var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles" ];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray = (array, element) {
-  chocolateBars.unshift ("reeses");
-  return ["reeses" , "snickers" , "hundred grand" , "kitkat" , "skittles"];
-};
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var Array = array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  var Array = array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index) {
+return 3
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+array.shift();
+return array
+}
+
+function removeElementFromBeginningOfArray(array, element) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop();
+return array
+}
+
+function removeElementFromEndOfArray (array) {
+  array = array.slice(0, array.length - 1);
+  return array;
+}
